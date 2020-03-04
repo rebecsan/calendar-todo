82fce553550c4ff99d31af10a7ebec4d387f683e
JosefinLunden <josefincslunden@hotmail.se>
2020-03-04 12:45:57 +0100

    Added a calendar.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+  </div>
+
 </template>
 
 <script>
-export default {};
+export default {
+};
+
 </script>
 
 <style>
---
+++ frontend/src/components/calendar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+  <form>
+    <label for="name">New activity:</label>
+    <input type="text" id="name" name="name">
+    <label for="dateStart">Date start:</label>
+    <input type="date" id="dateStart" name="dateStart">
+    <label for="dateEnd">Date end:</label>
+    <input type="date" id="dateEnd" name="dateEnd">
+
+    <label for="time">Select a starttime:</label>
+    <input type="time" id="startTime" name="startTime">
+    <label for="time">Select a endtime:</label>
+    <input type="time" id="endTime" name="endTime">
+    <label for="cars">Category:</label>
+
+        <select id="cars">
+          <option value="workout">Workout</option>
+          <option value="work">Work</option>
+          <option value="school">School</option>
+          <option value="vaycay">Vaycay</option>
+        </select>
+    <input type="submit" value="Lägg till">
+      
+  </form>
+
+  <ul>
+      <li v-for="activity in activities" :key="activity.id">{{activity}}</li>
+    </ul>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "App",
+  created() {
+    fetch("http://localhost:3000/")
+      .then(response => response.json())
+      .then(result => {
+        this.activities = result;
+      });
+  },
+  data() {
+    return {
+      activities: null
+    };
+  },
+};
+
+</script>
+
+<style>
+input [type=text], [type=date], [type=time], #name, #cars {
+  width: 30%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  
+}
+input[type=submit] {
+  width: 30%;
+  padding: 14px 20px;
+  margin: 8px 0px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+}
+form {
+  margin:4em;
+}
+</style>
