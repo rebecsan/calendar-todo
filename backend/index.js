8be6315f1a6f417c469f3a5b55c5fb8c37b04aab
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-03-10 09:29:27 +0100

    added POST requests with fetch (method: POST)

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,54 @@
+const express = require('express')
+const sqlite = require('sqlite')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+let database;
+
+sqlite.open('database.sqlite').then(database_ => {
+  database = database_
+})
+
+app.post('/budget', (request, response) => {
+  console.log(request.body);
+  database.run(
+      'INSERT INTO budget VALUES ($activity, $amount)', {
+        $activity: request.body.activity,
+        $amount: request.body.amount
+      })
+    .then(() => {
+      response.send()
+    })
+
+  // (err) => {
+  //   if (err) {
+  //     response.status(400)
+  //     response.send()
+  //   } else {
+  //     response.status(201)
+  //     response.send()
+  //   }
+  // }
+
+})
+
+app.get('/', (request, response) => {
+  database.all('SELECT * FROM budget').then(budget => {
+    console.log(budget)
+    response.send(budget)
+  })
+})
+
+app.listen(3000, () => {
+  console.log("server started");
+})
+
+
+/*
 const sqlite = require('sqlite')
 const express = require('express')
 const cors = require('cors')
@@ -11,37 +62,33 @@ let database
 // hämtar värden i en databas. 
 sqlite.open('database.sqlite').then(database_ => {
   database = database_
+})
+app.get('/', (request, response) => {
+  database.all('SELECT * FROM activities').then(activities => {
+    console.log(activities)
+    response.send(activities)
+
   })
-  app.get('/', (request, response) => {
-    database.all('SELECT * FROM activities').then(activities => {
-      console.log(activities)
-      response.send(activities) 
-      
+})
+
+app.post('/', (request, response) => {
+  database.run('INSERT INTO activities VALUES (?, ?, ?, ?, ?)',
+      [
+        request.body.name,
+        request.body.dateStart,
+        request.body.dateEnd,
+        request.body.timeStart,
+        request.body.timeEnd
+      ])
+    .then(() => {
+      response.send()
     })
-  })
 
-  app.post('/', (request, response) => {
-    database.run('INSERT INTO activities VALUES (?, ?, ?, ?, ?)', 
-    [
-    request.body.name,
-    request.body.dateStart,
-    request.body.dateEnd,
-    request.body.timeStart, 
-    request.body.timeEnd 
-    ])
-      .then(() => {
-        response.send() 
-          
-        
-      })
-    
-  
-    
-      // $name: request.body.name
-      // $population: request.body.population
-  
-  })
+  // $name: request.body.name
+  // $population: request.body.population
 
-
+})
 
 app.listen(3000)
+
+*/
