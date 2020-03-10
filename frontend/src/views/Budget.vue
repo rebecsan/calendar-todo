2d7c8190f5928dce129bf34d7c2db315e6cb3bfa
Diana Nilsson <diana.sofie.nilsson@gmail.com>
2020-03-10 10:12:04 +0100

    updated

--- frontend/src/views/Budget.vue
+++ frontend/src/views/Budget.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>
-    <p>Routerlänken till komponenten Budget.vue fungerar</p>
+  <div id="budget">
+    <form class="test-form">
+      <input type="text" v-model="addedActivity" />
+      <input type="text" v-model="addedAmount" />
+      <input type="submit" @click.prevent="onClick" />
+    </form>
+    <p>{{addedActivity}}: {{addedAmount}}</p>
+    <div id="status"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Budget"
+  name: "Budget",
+  data() {
+    return {
+      inputs: [],
+      addedActivity: null,
+      addedAmount: null
+    };
+  },
+  methods: {
+    onClick() {
+      this.postData();
+    },
+    postData() {
+      fetch("http://localhost:3000/budget", {
+        body:
+          '{"activity": "' +
+          this.addedActivity +
+          '"' +
+          ', "amount": "' +
+          this.addedAmount +
+          '"}',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    }
+  }
 };
 </script>
 
 <style scoped>
+.test-form {
+  padding: 1rem;
+}
+
+.test-form input {
+  border: 0.5px solid grey;
+  border-radius: 2px;
+  margin: 0.2rem;
+  padding: 0.2rem;
+}
 </style>
